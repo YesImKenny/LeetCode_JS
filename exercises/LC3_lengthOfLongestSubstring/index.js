3b65eea4a4c4614ffe70b9a887760f04f41a846f
Kenneth Trott <kentrottjr@gmail.com>
2019-09-17 16:13:07 -0700

    solved using O(n) time complexity

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,23 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let hashMap = {};
+  let start = 0;
+  let maxVar = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currentChar = s[i];
+
+    if (hashMap[currentChar] >= start) {
+      start = hashMap[currentChar] + 1;
+    }
+
+    hashMap[currentChar] = i;
+
+    maxVar = Math.max(maxVar, i - start + 1);
+  }
+  return maxVar;
+}
 
 module.exports = lengthOfLongestSubstring;
