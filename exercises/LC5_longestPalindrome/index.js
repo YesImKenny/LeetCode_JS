feb0f4184f725192c7960d8b00f6f0747c8d8d2f
Kenneth Trott <kentrottjr@gmail.com>
2020-01-06 10:40:16 -0800

    solved using O(n^2) time complexity

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,30 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let startIndex = 0;
+  let maxLength = 1;
 
+  function expandAroundMiddle(left, right) {
+    while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        maxLength = currentPalLength;
+        startIndex = left;
+      }
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundMiddle(i-1, i+1);
+    expandAroundMiddle(i, i+1);
+  }
+  return s.slice(startIndex, startIndex + maxLength);
+}
+
+//Time Complexity: O(n^2) Since expanding a palindrome around its center could take up to O(n), and we do this for each character.
+
+//Space Complexity: O(1)
 module.exports = longestPalindrome;
