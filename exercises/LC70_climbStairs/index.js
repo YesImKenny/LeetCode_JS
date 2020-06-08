c620fc3e0835db8c7bc046f5ad0612e4928e5814
Kenneth Trott <kentrottjr@gmail.com>
2020-06-08 10:04:23 -0700

    Solve with O(n) time complexity

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,3 +1,13 @@
-const climbStairs = n => {};
+const climbStairs = n => {
+  if (n <= 3) return n;
+  
+  let waysToStep = [0, 1, 2, 3];
+  
+  for (let i = 4; i <= n; i++) {
+    waysToStep.push(waysToStep[i-1] + waysToStep[i-2]);
+  }
+  
+  return waysToStep[n];
+};
 
 module.exports = climbStairs;
