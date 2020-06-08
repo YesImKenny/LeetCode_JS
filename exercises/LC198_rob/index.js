ec67f435e91ce4fe704e4be5f6990f30a9b68c0f
Kenneth Trott <kentrottjr@gmail.com>
2020-06-08 10:07:57 -0700

    Solve with O(n) time complexity

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,3 +1,14 @@
-function rob(nums) {}
+function rob(nums) {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
+  
+  let loot = [nums[0], Math.max(nums[0], nums[1])];
+  
+  for (let i = 2; i < nums.length; i++) {
+    loot.push(Math.max(loot[i-1], nums[i]+loot[i-2]))
+  }
+  return loot.pop();
+}
 
 module.exports = rob;
